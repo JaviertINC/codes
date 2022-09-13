7dc4680ab873be3b9ddbbef46edf1cb9a2c13ae9
[TXL] JaviertINC <txl@javiertinc.cl>
2022-09-13 19:13:00 -0300

    Added switch.php

---
+++ PHP/switch.php
@@ -0,0 +1,16 @@
+<?php
+switch($param){
+    case 0:
+        //Entero
+    break;
+    case "0":
+        //String
+    break;
+    case 1:
+    case "1":
+        //Dos entradas entero y String
+    break;
+    default:
+        //Si ninguna calza
+    break;
+}
